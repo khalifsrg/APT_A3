6d065d8d69905e27156cd291858c758ea1cb52f5
Khalif <khalif@Khalifs-MacBook-Pro.local>
2024-06-20 02:59:18 +1000

    Question 3 solved and running

--- question3/Date.cpp
+++ question3/Date.cpp
@@ -1,80 +1,249 @@
 #include "Date.h"
-#include <exception>
+#include <stdexcept>
 
-Date::Date():
-    year(1970), month(1), day(1)
-{    
+/// @brief Constructors
+Date::Date() : year(1970), month(1), day(1) {}
+
+Date::Date(unsigned int year, unsigned int month, unsigned int day) {
+    if (!isValidDate(year, month, day)) {
+        throw std::invalid_argument("Invalid date");
+    }
+    this->year = year;
+    this->month = month;
+    this->day = day;
 }
 
-Date::Date(unsigned int year, unsigned int month, unsigned int day):
-                        year(year), month(month), day(day)
-{    
+Date::Date(const Date& other) : year(other.year), month(other.month), day(other.day) {}
+
+Date::Date(Date&& other) noexcept : year(other.year), month(other.month), day(other.day) {}
+
+Date::Date(unsigned long dayNumber) {
+    setDate(dayNumber);
 }
 
-Date::Date(const Date& other){
-    year = other.year;
-    month = other.month;
-    day = other.day;
-}
+/// @brief Destructor
+Date::~Date() {}
 
-Date::Date(Date&& other){
-    year = other.year;
-    month = other.month;
-    day = other.day;
-}
-
-Date::~Date(){
-    
-}
-
-unsigned int Date::getDay(){
+// Getters
+unsigned int Date::getDay() const {
     return day;
 }
 
-unsigned int Date::getMonth(){
+unsigned int Date::getMonth() const {
     return month;
 }
 
-unsigned int Date::getYear(){
+unsigned int Date::getYear() const {
     return year;
 }
 
-
-unsigned long Date::getDayNumber() const{
-    unsigned long m = ((unsigned long)month + 9) % 12;
-    unsigned long y = (unsigned long)year - ((unsigned long)month/10);
-    return 365*y + y/4 - y/100 + y/400 
-                + (m*306 + 5)/10 + ( (unsigned long)day - 1 );
+unsigned long Date::getDayNumber() const {
+    return calculateDayNumber();
 }
 
-void Date::setDate(unsigned long dayNumber){
-    unsigned long y = (10000*((unsigned long)dayNumber) + 14780)/3652425;
-    unsigned long ddd = (unsigned long)dayNumber - (365*y + y/4 - y/100 + y/400);
-    if (ddd < 0){
+// Setters
+void Date::setDate(unsigned int year, unsigned int month, unsigned int day) {
+    if (!isValidDate(year, month, day)) {
+        throw std::invalid_argument("Invalid date!");
+    }
+    this->year = year;
+    this->month = month;
+    this->day = day;
+}
+
+void Date::setDate(unsigned long dayNumber) {
+    setDateFromDayNumber(dayNumber);
+}
+
+// Operators
+Date& Date::operator=(const Date& other) {
+    if (this != &other) {
+        year = other.year;
+        month = other.month;
+        day = other.day;
+    }
+    return *this;
+}
+
+Date& Date::operator=(Date&& other) {
+    if (this != &other) {
+        year = other.year;
+        month = other.month;
+        day = other.day;
+    }
+    return *this;
+}
+
+Date& Date::operator++() {
+    *this += 1;
+    return *this;
+}
+
+Date Date::operator++(int) {
+    Date temp(*this);
+    ++(*this);
+    return temp;
+}
+
+Date& Date::operator--() {
+    *this -= 1;
+    return *this;
+}
+
+Date Date::operator--(int) {
+    Date temp(*this);
+    --(*this);
+    return temp;
+}
+
+unsigned int& Date::operator[](const std::string& index) {
+    if (index == "year") {
+        return year;
+    } else if (index == "month") {
+        return month;
+    } else if (index == "day") {
+        return day;
+    } else {
+        throw std::out_of_range("Invalid index!");
+    }
+}
+
+Date& Date::operator+=(int numDays) {
+    unsigned long dayNum = calculateDayNumber();
+    dayNum += numDays;
+    setDateFromDayNumber(dayNum);
+    return *this;
+}
+
+Date& Date::operator-=(int numDays) {
+    unsigned long dayNum = calculateDayNumber();
+    dayNum -= numDays;
+    setDateFromDayNumber(dayNum);
+    return *this;
+}
+
+Date Date::operator+(int numDays) const {
+    Date result(*this);
+    result += numDays;
+    return result;
+}
+
+Date Date::operator-(int numDays) const {
+    Date result(*this);
+    result -= numDays;
+    return result;
+}
+
+// Comparison operators
+bool Date::operator==(const Date& other) const {
+    return year == other.year && month == other.month && day == other.day;
+}
+
+bool Date::operator!=(const Date& other) const {
+    return !(*this == other);
+}
+
+bool Date::operator<(const Date& other) const {
+    if (year < other.year)
+        return true;
+    if (year > other.year)
+        return false;
+    if (month < other.month)
+        return true;
+    if (month > other.month)
+        return false;
+    return day < other.day;
+}
+
+bool Date::operator>(const Date& other) const {
+    return other < *this;
+}
+
+bool Date::operator<=(const Date& other) const {
+    return !(other < *this);
+}
+
+bool Date::operator>=(const Date& other) const {
+    return !(*this < other);
+}
+
+/// @brief Subtract two dates to get the difference in days (Fix for error)
+/// @param other 
+/// @return the difference in days between the two given days
+int Date::operator-(const Date& other) const {
+    unsigned long thisDayNumber = getDayNumber();
+    unsigned long otherDayNumber = other.getDayNumber();
+
+    return (thisDayNumber >= otherDayNumber) ? static_cast<int>(thisDayNumber - otherDayNumber)
+                                            : -static_cast<int>(otherDayNumber - thisDayNumber);
+}
+
+/// @brief friend operator to output the information
+/// @param os 
+/// @param date 
+/// @return ostream
+std::ostream& operator<<(std::ostream& os, const Date& date) {
+    // Format date output in the desired format
+    os << date.getDay() << "/" << date.getMonth() << "/" << date.getYear();
+    return os;
+}
+
+// Helper functions
+
+/// @brief Calculates the number of days since "1st March 0000"
+/// @return the number of days since "1st March 0000"
+unsigned long Date::calculateDayNumber() const {
+    unsigned long m = (month + 9) % 12;
+    unsigned long y = year - m/10;
+    return 365*y + y/4 - y/100 + y/400 + (m*306 + 5)/10 + (day - 1);
+}
+
+/// @brief set the date based on the calculation from calculateDayNumber() 
+/// @param dayNumber Taken from the calculateDayNumber() function
+void Date::setDateFromDayNumber(unsigned long dayNumber) {
+    unsigned long y = (10000*dayNumber + 14780)/3652425;
+    unsigned long ddd = dayNumber - (365*y + y/4 - y/100 + y/400);
+    if (ddd < 0) {
         y = y - 1;
-        ddd = (unsigned long)dayNumber - (365*y + y/4 - y/100 + y/400);
+        ddd = dayNumber - (365*y + y/4 - y/100 + y/400);
     }
     unsigned long mi = (100*ddd + 52)/3060;
     unsigned long mm = (mi + 2)%12 + 1;
     y = y + (mi + 2)/12;
     unsigned long dd = ddd - (mi*306 + 5)/10 + 1;
 
-    this->day = (unsigned int)dd;
-    this->month = (unsigned int)mm;
-    this->year = (unsigned int)y;
+    year = static_cast<unsigned int>(y);
+    month = static_cast<unsigned int>(mm);
+    day = static_cast<unsigned int>(dd);
 }
 
-void Date::setDate(unsigned int year, unsigned int month, unsigned int day){
-    this->day = day;
-    this->month = month;
-    this->year = year;
+/// @brief Returns true or false whether or not the date is valid
+/// @param year The year to be checked
+/// @param month The month to be checked
+/// @param day The day to be checked
+/// @return Returns true if the dat is valid, otherwise false
+bool Date::isValidDate(unsigned int year, unsigned int month, unsigned int day) const {
+    if (year < 1 || month < 1 || month > 12)
+        return false;
+
+    unsigned int daysInThisMonth = daysInMonth(year, month);
+    return (day >= 1 && day <= daysInThisMonth);
 }
 
-
-Date::Date(unsigned long dayNumber){
-    this->setDate(dayNumber);
+/// @brief Checks if the given year is a leap year
+/// @param year The year to be checked
+/// @return Returns true if the year is a leap year, otherwise false
+bool Date::isLeapYear(unsigned int year) const {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
-/*                                           */
-/* YOU MAY ADD YOUR MODIFICATIONS HERE       */
-/*                                           */
+/// @brief Determines the number of days in a given month of a specified year
+/// @param year The year in which the month occurs
+/// @param month The month used to determine the number of days
+/// @return The number of days of the given month in the specified year
+unsigned int Date::daysInMonth(unsigned int year, unsigned int month) const {
+    static const unsigned int days[] = {31, 28, 31, 30, 31, 30,
+                                        31, 31, 30, 31, 30, 31};
+
+    return (month == 2 && isLeapYear(year)) ? 29 : days[month - 1];
+}
--- question3/Date.h
+++ question3/Date.h
@@ -1,54 +1,71 @@
-#ifndef COSC1076_A3_DATE_H
-#define COSC1076_A3_DATE_H
+#ifndef DATE_H
+#define DATE_H
 
 #include <iostream>
 
-class Date{
-public:    
-    
-    // Constructors/DeConstructors
+class Date {
+public:
+    // Constructors
     Date();
-
-    //dayNumber is the number of days since 1/3/0000 
-    Date(unsigned long dayNumber);
+    Date(unsigned int year, unsigned int month, unsigned int day);
     Date(const Date& other);
-    Date(Date&& other);
+    Date(Date&& other) noexcept;
+    Date(unsigned long dayNumber);
+
+    // Destructor
     ~Date();
 
-    //Contract: Year, Month, Day should be valid
-    //          Year should be a positive number, 
-    //          month should be a number in range [1,12], 
-    //          date should be a numbers in range [1,31] and valid. 
-    Date(unsigned int year, unsigned int month, unsigned int day);
-
-    
-    //Getters and setters
-    unsigned int getDay();
-    unsigned int getMonth();
-    unsigned int getYear();
+    // Getters
+    unsigned int getDay() const;
+    unsigned int getMonth() const;
+    unsigned int getYear() const;
     unsigned long getDayNumber() const;
 
+    // Setters
+    void setDate(unsigned int year, unsigned int month, unsigned int day);
     void setDate(unsigned long dayNumber);
 
-    //Contract: Year, Month, Day should be valid
-    //          Year should be a positive number, 
-    //          month should be a number in range [1,12], 
-    //          date should be a numbers in range [1,31] and valid. 
-    void setDate(unsigned int year, unsigned int month, unsigned int day);
+    // Operators
+    Date& operator=(const Date& other);
+    Date& operator=(Date&& other);
+    Date& operator++(); // prefix ++
+    Date operator++(int); // postfix ++
+    Date& operator--(); // prefix --
+    Date operator--(int); // postfix --
+    unsigned int& operator[](const std::string& index);
 
-    /*                                           */
-    /* YOU MAY ADD YOUR MODIFICATIONS HERE       */
-    /*                                           */
+    // Arithmetic operators
+    Date& operator+=(int numDays);
+    Date& operator-=(int numDays);
+    Date operator+(int numDays) const;
+    Date operator-(int numDays) const;
 
-    
+    // Comparison operators
+    bool operator==(const Date& other) const;
+    bool operator!=(const Date& other) const;
+    bool operator<(const Date& other) const;
+    bool operator>(const Date& other) const;
+    bool operator<=(const Date& other) const;
+    bool operator>=(const Date& other) const;
+
+    // Friend function to assist in outputting date
+    friend std::ostream& operator<<(std::ostream& os, const Date& date);
+
+    // - operator that was missing due to, only operator that is an int
+    int operator-(const Date& other) const;
+
 private:
-    /*                                           */
-    /* DO NOT MOFIFY ANY CODE IN THIS SECTION    */
-    /*                                           */
+    // Unmodified
     unsigned int year;
     unsigned int month;
     unsigned int day;
+
+    // Helper functions (Due to not having a Helper file)
+    unsigned long calculateDayNumber() const;
+    void setDateFromDayNumber(unsigned long dayNumber);
+    bool isValidDate(unsigned int year, unsigned int month, unsigned int day) const;
+    bool isLeapYear(unsigned int year) const;
+    unsigned int daysInMonth(unsigned int year, unsigned int month) const;
 };
 
-
-#endif //COSC1076_A3_DATE_H
+#endif // DATE_H
